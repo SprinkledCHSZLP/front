4f9e288ce941e4d86640dbb8c404b84a4094c187
SprinkledCHSZLP <mcosintsev@yandex.ru>
2023-04-19 23:39:16 +0300

    ff

--- CHSZiK/src/app/main/pages/list-of-equipment/layout/list-of-equipment.component.ts
+++ CHSZiK/src/app/main/pages/list-of-equipment/layout/list-of-equipment.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-list-of-equipment',
   templateUrl: './list-of-equipment.component.html',
   styleUrls: ['./list-of-equipment.component.scss'],
 })
-export class ListOfEquipmentComponent {}
+export class ListOfEquipmentComponent {
+
+  constructor(private route: Router) {
+  }
+
+  submitAddingEquipment() {
+    this.route.navigate(['adding-equipment'])
+  }
+}
